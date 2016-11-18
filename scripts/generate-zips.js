a9270accf8062a6067fe2e894e14582247cfc044
Belén Albeza <belen.albeza@gmail.com>
2016-11-18 17:33:08 +0100

    add zipping files of assignments

---
+++ scripts/generate-zips.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var archiver = require('archiver');
+var path = require('path');
+var fs = require('fs');
+
+// add here all the dirs that will be zipped
+const dirs = [
+    'es/03-javascript-en-el-navegador/03-practica/start-here'
+];
+
+dirs.forEach(function (src) {
+    let zipFile = archiver('zip');
+    let filename = path.join(path.dirname(src), `${path.basename(src)}.zip`);
+
+    let output = fs.createWriteStream(filename);
+    output.on('close', function () {
+        console.log(`${filename} - ${zipFile.pointer()} total bytes`);
+    });
+
+    zipFile.pipe(output);
+    zipFile.glob('**/*', {
+        expand: true,
+        cwd: src,
+        dot: true
+    });
+    zipFile.finalize();
+});
