2d8b0c36423e915d426dd06af41b69ce1ab55204
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-03-27 23:59:41 +0100

    Tema 2.start-here. Correct small typo in the CharacterView's tests

--- es/02-javascript/02-practica/start-here/spec/CharacterView.js
+++ es/02-javascript/02-practica/start-here/spec/CharacterView.js
@@ -1,6 +1,6 @@
 var samples = require('./samplelib');
 
-xdescribe('CharactesView type', function () {
+xdescribe('CharactersView type', function () {
   'use strict';
 
   var CharactersView = require('../src/CharactersView');
